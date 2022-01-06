a63e584349031eb4b1f1f65386153b0e57248fcd
Sebastián González <seba.gnh@hgmail.com>
2022-01-06 18:16:58 -0300

    api funcionando

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Práctica utilizando la API "Intersection Observer"
 
 Git Hub Pages Link "https://sebagnh.github.io/Intersection-Observer/"
+
--- js/app.js
+++ js/app.js
@@ -2,11 +2,20 @@ const imagen1 = document.getElementById("imagen1");
 const imagen2 = document.getElementById("imagen2");
 
 
-const cargarImagen = () =>{
-    console.log("Ejecuta");
+const cargarImagen = (entradas, observador) =>{
+    //console.log(entradas); /* arreglo <-- contiene info de los elementos que está vigilando*/
+    //console.log(observador);
+    entradas.forEach((entrada)=>{ /*  <-- Identificamos cada entrada */
+        if (entrada.isIntersecting) { /*  <-- si la imagen está en el viewport */
+            //console.log("La imagen está en el viewport completamente");
+            entrada.target.classList.add("visible");
+        }else{
+            //entrada.target.classList.remove("visible");
+        }
+    })
 }
 
-const observador = new IntersectionObserver(cargarImagen, {
+const observador = new IntersectionObserver(cargarImagen, { /*  <-- Se ejecuta cada vez que una imagen entre y salga */
     root: null, /*  <-- Null hace referencia al viewport */
     rootMargin: '0px 0px 0px 0px',/*  <-- Margen en el que se ejecutará el código */
     threshold: 1.0, /*  <-- Ejecuta solo cuando esté toda la imagen en el viewport, va del 0.0 al 1.0, es el tamaño al ejecutarse */
@@ -15,3 +24,4 @@ const observador = new IntersectionObserver(cargarImagen, {
 });
 
 observador.observe(imagen1); /*  <-- le ponemos la ruta de la imagen */
+observador.observe(imagen2);
